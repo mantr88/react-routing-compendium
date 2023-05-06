d49858ef9ea4914101af77d026c2ded90c294532
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-05-06 22:50:54 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
+import { Route, Routes } from 'react-router-dom';
+import { About } from '../pages/About';
+import { Home } from '../pages/Home';
+import { ProductDetails } from '../pages/ProductDetails';
+import { Products } from '../pages/Products';
+import { NotFound } from '../pages/NotFound';
+import { Container, Header, Logo, Link } from './App.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Header>
+        <Logo>
+          <span role="img" aria-label="computer icon">
+            💻
+          </span>{' '}
+          GoMerch Store
+        </Logo>
+        <nav>
+          <Link to="/" end>
+            Home
+          </Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+      </Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Container>
   );
 };
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+export const NotFound = () => {
+  return (
+    <main>
+      <h1>Page not found!🥲</h1>
+      <p>Please try enter another URL</p>
+    </main>
+  );
+};
